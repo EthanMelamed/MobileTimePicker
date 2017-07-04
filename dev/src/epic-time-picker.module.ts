9532331ebc16cf9b71d1a9bf46966767f929508b
CORP\melamee <melamee@navcanada.ca>
2017-07-04 16:29:42 -0400

    testing removed translation library

--- dev/src/epic-time-picker.module.ts
+++ dev/src/epic-time-picker.module.ts
@@ -13,28 +13,10 @@
     import { TimePickerModal } from './time-picker.modal';
 
 
-//NGX-TRANSLATE
-    import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-    import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-
-    // AoT requires an exported function for factories
-    export function HttpLoaderFactory(http: Http) {
-        return new TranslateHttpLoader(http, "../assets/i18n/", ".json");
-    }
-
     @NgModule({
       imports: [
         CommonModule,
         MaterialModule.forRoot(),
-        //TRANSLATE
-        TranslateModule.forChild({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [Http]
-            }
-        })
       ],
       exports: [
         TimePickerComponent,
